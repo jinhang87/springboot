001c37d9c74f115d9416e24922d00074e7d8f9ed
jinhang <jinhang87@163.com>
2018-06-17 14:50:54 +0800

    [ADD]spring boot druid脚手架代码；

---
+++ druid/src/main/java/com/example/druid/Config/DruidConfig.java
@@ -0,0 +1,16 @@
+package com.example.druid.Config;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.ServletRegistration;
+
+@Configuration
+public class DruidConfig {
+
+//    @Bean
+//    public ServletRegistrationBean druidServlet(){
+//        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new StatV)
+//    }
+}
