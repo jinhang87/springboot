7adff9f1c7749183a653a0a207039faaad069ecf
jinhang <jinhang87@163.com>
2019-06-02 18:11:12 +0800

    [ADD] spring boot 整合 vue 整体打包工程；

---
+++ springboot-vue-integration/src/main/java/com/example/springbootvueintegration/SpringbootVueIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootvueintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootVueIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootVueIntegrationApplication.class, args);
+	}
+
+}
